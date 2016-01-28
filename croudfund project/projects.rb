3459900b6e4221e3553620b1583aec74ed2fc844
Aymen Chetoui <ach@office.123hjemmeside.local>
2016-01-28 23:14:18 +0100

    15%

---
+++ croudfund project/projects.rb
@@ -0,0 +1,11 @@
+# Project funding program
+
+fund = 1000
+project = "ABC"
+
+puts "Project #{project} has $#{fund} in funding."
+
+project1 = 'ABC'
+project2 = 'DEF'
+project3 = 'GHI'
+puts "Projects : \n\t#{project1}\n\t#{project2}\n\t#{project3}"
---
+++ file.rb
@@ -0,0 +1,5 @@
+comment = 'Ruby is fun'
+puts Time.new
+3.times do
+  puts comment
+end
